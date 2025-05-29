acddcef10119d62ef7db63bd14c99680c27bf05b
Ömer İslamoğlu <omer@trinsyca.com>
2025-05-29 13:07:08 +0300

    Main page and gamelist menu frontend. Included league, valorant, netflix logos and fonts.

--- resources/views/gamelist.blade.php
+++ resources/views/gamelist.blade.php
@@ -7,35 +7,159 @@
             </div>
         </div>
         <div class="game-list-container">
-            <div class="game-item">
-                <h3>Sihirdar Vadisi</h3>
-                <h5>League Of Legends</h5>
-                <p>Oluşturulma: {{ date('h:i:s d/m/Y') }}</p>
-            </div>
-            <div class="game-item">
-                <h3>Kırağı Çemberi</h3>
-                <h5>League Of Legends</h5>
-                <p>Oluşturulma: {{ date('h:i:s d/m/Y') }}</p>
-            </div>
-            <div class="game-item">
-                <h3>Bandle Şehri</h3>
-                <h5>League Of Legends</h5>
-                <p>Oluşturulma: {{ date('h:i:s d/m/Y') }}</p>
-            </div>
-            <div class="game-item">
-                <h3>Haven</h3>
-                <h5>Valorant</h5>
-                <p>Oluşturulma: {{ date('h:i:s d/m/Y') }}</p>
-            </div>
-            <div class="game-item">
-                <h3>Project: A</h3>
-                <h5>Valorant</h5>
-                <p>Oluşturulma: {{ date('h:i:s d/m/Y') }}</p>
-            </div>
-            <div class="game-item">
-                <h3>Bynie</h3>
-                <h5>Dexter</h5>
-                <p>Oluşturulma: {{ date('h:i:s d/m/Y') }}</p>
+            <div class="game-list-container-table">
+                <div class="game-item">
+                    <div class="game-item-flex">
+                        <div class="game-item-title">
+                            <b font="leagueoflegends">Sihirdar Vadisi</b>
+                            <img src="{{ asset('assets/images/categories/leagueoflegends/rectangle-new-logo.webp') }}" alt="League of Legends Logo" class="game-item-rectangle">
+                        </div>
+                        <div class="game-item-user-details">
+                            <div class="game-item-logo-side">
+                                <img src="{{ asset('assets/images/categories/leagueoflegends/shortcut-icon.webp') }}" alt="League of Legends Logo" class="game-logo">
+                            </div>
+                            <br>
+                            <div class="game-item-users">
+                                <div class="game-item-users-container">
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 1" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 2" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                </div>
+                                <div class="game-item-users-count">
+                                    <span>5 Kişi Oyunda</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="game-item-createtime">Round: 5</p>
+                    <p class="game-item-createtime">Soru: naber nasılsın burnun kapıya kısılsın.</p>
+                </div>
+                <div class="game-item">
+                    <div class="game-item-flex">
+                        <div class="game-item-title">
+                            <b font="leagueoflegends">Sihirdar Vadisi</b>
+                            <img src="{{ asset('assets/images/categories/leagueoflegends/rectangle-new-logo.webp') }}" alt="League of Legends Logo" class="game-item-rectangle">
+                        </div>
+                        <div class="game-item-user-details">
+                            <div class="game-item-logo-side">
+                                <img src="{{ asset('assets/images/categories/leagueoflegends/shortcut-icon.webp') }}" alt="League of Legends Logo" class="game-logo">
+                            </div>
+                            <br>
+                            <div class="game-item-users">
+                                <div class="game-item-users-container">
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 1" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 2" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                </div>
+                                <div class="game-item-users-count">
+                                    <span>5 Kişi Oyunda</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="game-item-createtime">Round: 3</p>
+                    <p class="game-item-createtime">Soru: Dehşet purna.</p>
+                </div>
+                <div class="game-item">
+                    <div class="game-item-flex">
+                        <div class="game-item-title">
+                            <b font="leagueoflegends">Sihirdar Vadisi</b>
+                            <img src="{{ asset('assets/images/categories/leagueoflegends/rectangle-new-logo.webp') }}" alt="League of Legends Logo" class="game-item-rectangle">
+                        </div>
+                        <div class="game-item-user-details">
+                            <div class="game-item-logo-side">
+                                <img src="{{ asset('assets/images/categories/leagueoflegends/shortcut-icon.webp') }}" alt="League of Legends Logo" class="game-logo">
+                            </div>
+                            <br>
+                            <div class="game-item-users">
+                                <div class="game-item-users-container">
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 1" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 2" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                </div>
+                                <div class="game-item-users-count">
+                                    <span>5 Kişi Oyunda</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="game-item-createtime">Round: 1</p>
+                    <p class="game-item-createtime">Soru: Merhaba ben ferit wristcutter karakaya.</p>
+                </div>
+                <div class="game-item">
+                    <div class="game-item-flex">
+                        <div class="game-item-title">
+                            <b font="leagueoflegends">Sihirdar Vadisi</b>
+                            <img src="{{ asset('assets/images/categories/leagueoflegends/rectangle-new-logo.webp') }}" alt="League of Legends Logo" class="game-item-rectangle">
+                        </div>
+                        <div class="game-item-user-details">
+                            <div class="game-item-logo-side">
+                                <img src="{{ asset('assets/images/categories/leagueoflegends/shortcut-icon.webp') }}" alt="League of Legends Logo" class="game-logo">
+                            </div>
+                            <br>
+                            <div class="game-item-users">
+                                <div class="game-item-users-container">
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 1" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 2" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                    <div class="game-item-user">
+                                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}" alt="User 3" class="game-item-user-image">
+                                    </div>
+                                </div>
+                                <div class="game-item-users-count">
+                                    <span>5 Kişi Oyunda</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="game-item-createtime">Round: 6</p>
+                    <p class="game-item-createtime">Soru: Camille'in ayak numarası kaçtır?</p>
+                </div>
             </div>
         </div>
     </div>
@@ -43,19 +167,31 @@
         <div class="gamelist-category">
             <div class="gamelist-category-title">
                 <img src="{{ asset('assets/images/categories/leagueoflegends/shortcut-icon.webp') }}" alt="League of Legends Logo" class="game-logo">
-                <h3>League Of Legends</h3>
+                <h3 font="leagueoflegends">League Of Legends</h3>
             </div>
             <p>Anlık Oyuncu: 258</p>
             <p><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><rect width="32" height="32" fill="none"/><g fill="none"><path fill="#ffb02e" d="M15.999 29.998c9.334 0 13.999-6.268 13.999-14c0-7.73-4.665-13.998-14-13.998C6.665 2 2 8.268 2 15.999s4.664 13.999 13.999 13.999"/><path fill="#ff6723" d="M11 16c0 1.105-1.343 2-3 2s-3-.895-3-2s1.343-2 3-2s3 .895 3 2m16 0c0 1.105-1.343 2-3 2s-3-.895-3-2s1.343-2 3-2s3 .895 3 2"/><path fill="#402a32" d="M6.974 9.658c.144-.43.502-1.108 1.095-1.67C8.654 7.433 9.452 7 10.5 7a.5.5 0 0 0 0-1c-1.351 0-2.386.567-3.118 1.262c-.724.688-1.166 1.51-1.355 2.08a.5.5 0 0 0 .948.316m16.955-1.67c.594.562.952 1.24 1.096 1.67a.5.5 0 1 0 .948-.316c-.19-.57-.63-1.392-1.355-2.08C23.887 6.567 22.852 6 21.5 6a.5.5 0 0 0 0 1c1.048 0 1.846.433 2.43.988M8.982 13.19c.048-.246.158-.55.367-.777c.18-.196.498-.413 1.15-.413c.643 0 .97.222 1.158.429c.218.24.323.545.358.742a1 1 0 0 0 1.97-.342a3.54 3.54 0 0 0-.85-1.747C12.563 10.452 11.696 10 10.5 10c-1.184 0-2.047.431-2.624 1.06c-.548.596-.769 1.293-.858 1.75a1 1 0 1 0 1.964.38m11 0c.048-.246.158-.55.367-.777c.18-.196.498-.413 1.151-.413c.642 0 .969.222 1.157.429c.219.24.324.545.358.742a1 1 0 0 0 1.97-.342a3.54 3.54 0 0 0-.85-1.747C23.563 10.452 22.696 10 21.5 10c-1.184 0-2.047.431-2.624 1.06c-.548.596-.769 1.293-.857 1.75a1 1 0 1 0 1.963.38M9.4 18.2a1 1 0 0 1 1.4.2c.341.455 2.062 2.1 5.2 2.1s4.859-1.645 5.2-2.1a1 1 0 1 1 1.6 1.2c-.659.878-2.938 2.9-6.8 2.9s-6.141-2.022-6.8-2.9a1 1 0 0 1 .2-1.4"/></g></svg> Daha Önce Oynadın</p>
         </div>
         <div class="gamelist-category">
-            <h3>Valorant</h3>
-            <p>Anlık Oyuncu: 48</p>
+            <div class="gamelist-category-title">
+                <img src="{{ asset('assets/images/categories/valorant/shortcut-icon.webp') }}" alt="Valorant Logo" class="game-logo">
+                <h3 font="valorant">Valorant</h3>
+            </div>
+            <p>Anlık Oyuncu: 58</p>
             <p><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><rect width="64" height="64" fill="none"/><path fill="#ffdd67" d="M2 32c0 16.6 13.4 30 30 30s30-13.4 30-30S48.6 2 32 2S2 15.4 2 32"/><path fill="#65b1ef" d="M47.7 37.6c0 8.6 11.7 8.6 11.7 0c0-6.3-5.8-12.4-5.8-12.4s-5.9 6.1-5.9 12.4"/><g fill="#664e27"><circle cx="38.5" cy="35" r="5"/><circle cx="15.5" cy="35" r="5"/></g><path fill="#917524" d="M20.6 19.9c-3.2 2.7-7.5 3.9-11.7 3.1c-.6-.1-1.1 2-.4 2.2c4.8.9 9.8-.5 13.5-3.6c.5-.5-1-2.1-1.4-1.7m24.5 3c-4.2.7-8.5-.4-11.7-3.1c-.4-.4-2 1.2-1.4 1.7c3.7 3.2 8.7 4.5 13.5 3.6c.7-.2.2-2.3-.4-2.2"/><path fill="#664e27" d="M38.1 47.3c-5.8-1.5-12-.4-16.9 3c-1.2.9 1.1 4 2.3 3.2c3.2-2.3 8.4-3.8 13.7-2.4c1.3.3 2.4-3.3.9-3.8"/></svg> Hiç Denemedin</p>
         </div>
         <div class="gamelist-category">
-            <h3>Dexter</h3>
-            <p>Anlık Oyuncu: 15</p>
+            <div class="gamelist-category-title">
+                <img src="{{ asset('assets/images/categories/marvel/shortcut-icon.webp') }}" alt="Marvel Logo" class="game-logo rectangle">
+            </div>
+            <p>Anlık Oyuncu: 153</p>
+            <p><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><rect width="64" height="64" fill="none"/><path fill="#ffdd67" d="M2 32c0 16.6 13.4 30 30 30s30-13.4 30-30S48.6 2 32 2S2 15.4 2 32"/><path fill="#65b1ef" d="M47.7 37.6c0 8.6 11.7 8.6 11.7 0c0-6.3-5.8-12.4-5.8-12.4s-5.9 6.1-5.9 12.4"/><g fill="#664e27"><circle cx="38.5" cy="35" r="5"/><circle cx="15.5" cy="35" r="5"/></g><path fill="#917524" d="M20.6 19.9c-3.2 2.7-7.5 3.9-11.7 3.1c-.6-.1-1.1 2-.4 2.2c4.8.9 9.8-.5 13.5-3.6c.5-.5-1-2.1-1.4-1.7m24.5 3c-4.2.7-8.5-.4-11.7-3.1c-.4-.4-2 1.2-1.4 1.7c3.7 3.2 8.7 4.5 13.5 3.6c.7-.2.2-2.3-.4-2.2"/><path fill="#664e27" d="M38.1 47.3c-5.8-1.5-12-.4-16.9 3c-1.2.9 1.1 4 2.3 3.2c3.2-2.3 8.4-3.8 13.7-2.4c1.3.3 2.4-3.3.9-3.8"/></svg> Hiç Denemedin</p>
+        </div>
+        <div class="gamelist-category">
+            <div class="gamelist-category-title">
+                <img src="{{ asset('assets/images/categories/netflix/shortcut-icon.webp') }}" alt="Netflix Logo" class="game-logo rectangle">
+            </div>
+            <p>Anlık Oyuncu: 153</p>
             <p><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><rect width="64" height="64" fill="none"/><path fill="#ffdd67" d="M2 32c0 16.6 13.4 30 30 30s30-13.4 30-30S48.6 2 32 2S2 15.4 2 32"/><path fill="#65b1ef" d="M47.7 37.6c0 8.6 11.7 8.6 11.7 0c0-6.3-5.8-12.4-5.8-12.4s-5.9 6.1-5.9 12.4"/><g fill="#664e27"><circle cx="38.5" cy="35" r="5"/><circle cx="15.5" cy="35" r="5"/></g><path fill="#917524" d="M20.6 19.9c-3.2 2.7-7.5 3.9-11.7 3.1c-.6-.1-1.1 2-.4 2.2c4.8.9 9.8-.5 13.5-3.6c.5-.5-1-2.1-1.4-1.7m24.5 3c-4.2.7-8.5-.4-11.7-3.1c-.4-.4-2 1.2-1.4 1.7c3.7 3.2 8.7 4.5 13.5 3.6c.7-.2.2-2.3-.4-2.2"/><path fill="#664e27" d="M38.1 47.3c-5.8-1.5-12-.4-16.9 3c-1.2.9 1.1 4 2.3 3.2c3.2-2.3 8.4-3.8 13.7-2.4c1.3.3 2.4-3.3.9-3.8"/></svg> Hiç Denemedin</p>
         </div>
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,11 @@
     <h1 class="font-logo">Ben Kimim?</h1>
     @include('gamelist')
     <div class="home-content">
-        <p>Ben Kimim? oyun platformuna hoş geldiniz! Burada arkadaşlarınızla birlikte eğlenceli oyunlar oynayabilir, yeni arkadaşlıklar kurabilir ve rekabet dolu anlar yaşayabilirsiniz. Hemen bir oyun oluşturun veya mevcut bir oyuna katılın!</p>
-        <p>Platformumuz, League of Legends, Valorant ve daha birçok popüler oyunu destekler. Oyunlarınızı kolayca yönetin, arkadaşlarınızla iletişim kurun ve en iyi oyun deneyimini yaşayın.</p>
-        <p>Hemen başlayın ve oyun dünyasına adım atın!</p>
+        <p>Ben Kimim? ile eğlencenin sınırlarını zorla! Arkadaşlarınla veya yeni tanıştığın oyuncularla bir araya gel, zekanı konuştur ve unutulmaz anlar yaşa. League of Legends kahramanlarından popüler dizi ve film karakterlerine kadar her şeyi tahmin et, en iyi tahminci sen ol!</p>
+        <br>
+        <p>Platformumuzda League of Legends, Valorant, Stranger Things, Marvel evreni ve daha fazlasından ilham alan oyunlar seni bekliyor. Kendi oyun masanı oluştur, kategorini seç ve ister herkese açık ister özel odalarda rekabete başla. Sorular sor, ipuçlarını yakala ve liderlik tablosunda zirveye tırman!</p>
+        <br>
+        <p>Hemen bir oyuna katıl veya kendi maceranı başlat. Zekan, hızın ve eğlencenle Ben Kimim? dünyasında fark yarat!</p>
     </div>
 </section>
 @endsection

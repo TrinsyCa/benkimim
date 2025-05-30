48845200e70e7bf5d8173ab0a2f990e9916ec1ab
Ömer İslamoğlu <omer@trinsyca.com>
2025-05-30 21:55:40 +0300

    404 error page's title

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Anasayfa | Ben Kimim?')
+@section('title', 'Hata Sayfası | Ben Kimim?')
 @section('description', 'Ben Kimim? oyun platformuna hoş geldiniz! Burada arkadaşlarınızla birlikte eğlenceli oyunlar oynayabilir, yeni arkadaşlıklar kurabilir ve rekabet dolu anlar yaşayabilirsiniz. Hemen bir oyun oluşturun veya mevcut bir oyuna katılın!')
 @section('keywords', 'Ben Kimim, oyun platformu, arkadaşlar, oyunlar, League of Legends, Valorant, oyun oluşturma, oyun katılma')
 @section('author', 'Ben Kimim? Oyun Platformu')

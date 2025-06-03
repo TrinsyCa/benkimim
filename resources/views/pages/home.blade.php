f9aa51b945888af2306cbea51b8207ec26735189
Ömer İslamoğlu <omer@trinsyca.com>
2025-06-04 02:44:38 +0300

    clear home-content for now

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,11 +14,6 @@
     <h1 class="font-logo">Ben Kimim?</h1>
     @include('layouts.gamelist')
     <div class="home-content">
-        <p>Ben Kimim? ile eğlencenin sınırlarını zorla! Arkadaşlarınla veya yeni tanıştığın oyuncularla bir araya gel, zekanı konuştur ve unutulmaz anlar yaşa. League of Legends kahramanlarından popüler dizi ve film karakterlerine kadar her şeyi tahmin et, en iyi tahminci sen ol!</p>
-        <br>
-        <p>Platformumuzda League of Legends, Valorant, Stranger Things, Marvel evreni ve daha fazlasından ilham alan oyunlar seni bekliyor. Kendi oyun masanı oluştur, kategorini seç ve ister herkese açık ister özel odalarda rekabete başla. Sorular sor, ipuçlarını yakala ve liderlik tablosunda zirveye tırman!</p>
-        <br>
-        <p>Hemen bir oyuna katıl veya kendi maceranı başlat. Zekan, hızın ve eğlencenle Ben Kimim? dünyasında fark yarat!</p>
     </div>
 </section>
 @endsection

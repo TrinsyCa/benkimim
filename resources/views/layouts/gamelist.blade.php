e6d254a5fce0055d00ab14fd3abe5dfae5fabf05
Ömer İslamoğlu <omer@trinsyca.com>
2025-06-26 18:40:39 +0300

    sign buttons menu

--- resources/views/layouts/gamelist.blade.php
+++ resources/views/layouts/gamelist.blade.php
@@ -675,21 +675,27 @@
     </div>
     <div class="gamelist-sidebar">
         <div class="gamelist-profile-menu">
-            <div class="gamelist-profile" id="gamelistProfile">
-                <div class="gamelist-profile-top">
-                    <div class="gamelist-profile-picture">
-                        <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}?v={{ filemtime(public_path('assets/images/users/profilepic/user-1.webp')) }}" alt="User Profile">
-                    </div>
-                    <div class="gamelist-profile-details">
-                        <h3>TrinsyCa</h3>
-                        <p class="profileQuestTrigger"><img src="{{ asset('assets/images/rank/walter-white.webp') }}"><span>Let's Cook</span></p>
-                    </div>
+            <div class="gamelist-profile-menu-container">
+                <div class="gamelist-profile-account-btn">
+                    <button id="signup">Kayıt Ol</button>
+                    <button id="signin">Giriş Yap</button>
                 </div>
-                <div class="gamelist-profile-rank-container">
-                    <div class="gamelist-profile-rank" id="gamelistProfileMenu">
-                        <p font="leagueoflegends"><img src="{{ asset("assets/images/rank/diamond.webp") }}" alt="Elmas Elo" class="gamelist-profile-rank-img"> Elmas</p>
-                        <button id="settingsSecBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#fff" fill-rule="evenodd" d="M8 7a4 4 0 1 1 8 0a4 4 0 0 1-8 0m0 6a5 5 0 0 0-5 5a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3a5 5 0 0 0-5-5z" clip-rule="evenodd"/></svg><span>Profili Görüntüle</span></button>
-                        <button id="settingsSecBtnClose"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#fff" d="m4 8l-.707.707L2.586 8l.707-.707zm5 12a1 1 0 1 1 0-2zm-.707-6.293l-5-5l1.414-1.414l5 5zm-5-6.414l5-5l1.414 1.414l-5 5zM4 7h10.5v2H4zm10.5 13H9v-2h5.5zm6.5-6.5a6.5 6.5 0 0 1-6.5 6.5v-2a4.5 4.5 0 0 0 4.5-4.5zM14.5 7a6.5 6.5 0 0 1 6.5 6.5h-2A4.5 4.5 0 0 0 14.5 9z"/></svg><span>Geri Dön</span></button>
+                <div class="gamelist-profile" id="gamelistProfile">
+                    <div class="gamelist-profile-top">
+                        <div class="gamelist-profile-picture">
+                            <img src="{{ asset('assets/images/users/profilepic/user-1.webp') }}?v={{ filemtime(public_path('assets/images/users/profilepic/user-1.webp')) }}" alt="User Profile">
+                        </div>
+                        <div class="gamelist-profile-details">
+                            <h3>TrinsyCa</h3>
+                            <p class="profileQuestTrigger"><img src="{{ asset('assets/images/rank/walter-white.webp') }}"><span>Let's Cook</span></p>
+                        </div>
+                    </div>
+                    <div class="gamelist-profile-rank-container">
+                        <div class="gamelist-profile-rank" id="gamelistProfileMenu">
+                            <p font="leagueoflegends"><img src="{{ asset("assets/images/rank/diamond.webp") }}" alt="Elmas Elo" class="gamelist-profile-rank-img"> Elmas</p>
+                            <button id="settingsSecBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#fff" fill-rule="evenodd" d="M8 7a4 4 0 1 1 8 0a4 4 0 0 1-8 0m0 6a5 5 0 0 0-5 5a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3a5 5 0 0 0-5-5z" clip-rule="evenodd"/></svg><span>Profili Görüntüle</span></button>
+                            <button id="settingsSecBtnClose"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#fff" d="m4 8l-.707.707L2.586 8l.707-.707zm5 12a1 1 0 1 1 0-2zm-.707-6.293l-5-5l1.414-1.414l5 5zm-5-6.414l5-5l1.414 1.414l-5 5zM4 7h10.5v2H4zm10.5 13H9v-2h5.5zm6.5-6.5a6.5 6.5 0 0 1-6.5 6.5v-2a4.5 4.5 0 0 0 4.5-4.5zM14.5 7a6.5 6.5 0 0 1 6.5 6.5h-2A4.5 4.5 0 0 0 14.5 9z"/></svg><span>Geri Dön</span></button>
+                        </div>
                     </div>
                 </div>
             </div>
